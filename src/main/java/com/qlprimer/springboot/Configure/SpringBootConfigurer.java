f8db43348184d2cf1c2deb5c00ca59dd34e84202
qinlei <qinlei@hisign.com.cn>
2018-08-02 13:07:57 +0800

    8月2日 freemarker

--- src/main/java/com/qlprimer/springboot/Configure/SpringBootConfigurer.java
+++ src/main/java/com/qlprimer/springboot/Configure/SpringBootConfigurer.java
@@ -3,6 +3,7 @@ package com.qlprimer.springboot.Configure;
 import com.qlprimer.springboot.Interceptors.LoginIntercept;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceView;
@@ -16,5 +17,9 @@ public class SpringBootConfigurer implements WebMvcConfigurer {
         registry.addInterceptor(new LoginIntercept()).addPathPatterns("/**").excludePathPatterns("/ws");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static");
 
+    }
 }
